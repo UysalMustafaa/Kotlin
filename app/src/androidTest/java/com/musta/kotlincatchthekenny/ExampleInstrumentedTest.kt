fdd74f82a924a5d669e66b34e24b56e6530f7ec5
Mustafa UYSAL <uysall.mustafaa@gmail.com>
2021-06-27 22:44:49 +0300

    Update

--- app/src/androidTest/java/com/musta/kotlincatchthekenny/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/musta/kotlincatchthekenny/ExampleInstrumentedTest.kt
@@ -1,12 +1,3 @@
-package com.musta.kotlincatchthekenny
-
-import androidx.test.platform.app.InstrumentationRegistry
-import androidx.test.ext.junit.runners.AndroidJUnit4
-
-import org.junit.Test
-import org.junit.runner.RunWith
-
-import org.junit.Assert.*
 
 @RunWith(AndroidJUnit4::class)
 class ExampleInstrumentedTest {
